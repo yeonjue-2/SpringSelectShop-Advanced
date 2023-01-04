e95d3f15f31d929ad42618a0976e1bdb807f1515
yeonjue-2 <ilov-@naver.com>
2023-01-04 21:34:59 +0900

    feat: Product에 시큐리티 적용

--- src/main/java/com/example/springselectshop/service/ProductService.java
+++ src/main/java/com/example/springselectshop/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.springselectshop.service;
 
+
 import com.example.springselectshop.dto.ProductMypriceRequest;
 import com.example.springselectshop.dto.ProductRequest;
 import com.example.springselectshop.dto.ProductResponse;
@@ -7,12 +8,9 @@ import com.example.springselectshop.entity.Folder;
 import com.example.springselectshop.entity.Product;
 import com.example.springselectshop.entity.User;
 import com.example.springselectshop.entity.UserRoleEnum;
-import com.example.springselectshop.jwt.JwtUtil;
 import com.example.springselectshop.naver.ItemDto;
 import com.example.springselectshop.repository.FolderRepository;
 import com.example.springselectshop.repository.ProductRepository;
-import com.example.springselectshop.repository.UserRepository;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,132 +19,63 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    private final ProductRepository productRepository;
-    private final UserRepository userRepository;
     private final FolderRepository folderRepository;
-    private final JwtUtil jwtUtil;
+    private final ProductRepository productRepository;
 
     @Transactional
-    public ProductResponse createProduct(ProductRequest requestDto, HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;   // JWT 내 정보를 담을 수 있는 객체라고 생각하기
+    public ProductResponse createProduct(ProductRequest requestDto, User user) {
+        System.out.println("ProductService.createProduct");
+        System.out.println("user.getUsername() = " + user.getUsername());
 
-        // 토큰이 있는 경우에만 관심상품 추가 가능
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = productRepository.saveAndFlush(new Product(requestDto, user.getId()));
 
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-
-            // 요청받은 DTO 로 DB에 저장할 객체 만들기
-            Product product = productRepository.saveAndFlush(new Product(requestDto, user.getId()));
-
-            return new ProductResponse(product);
-        } else {
-            return null;
-        }
+        return new ProductResponse(product);
     }
 
-
-
     @Transactional(readOnly = true)
-    public Page<Product> getProducts(HttpServletRequest request,
+    public Page<Product> getProducts(User user,
                                      int page, int size, String sortBy, boolean isAsc) {
         // 페이징 처리
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
         Pageable pageable = PageRequest.of(page, size, sort);
+        // 사용자 권한 가져와서 ADMIN 이면 전체 조회, USER 면 본인이 추가한 부분 조회
+        UserRoleEnum userRoleEnum = user.getRole();
 
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+        Page<Product> products;
 
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
-
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-
-            // 사용자 권한 가져와서 ADMIN 이면 전체 조회, USER 면 본인이 추가한 부분 조회
-            UserRoleEnum userRoleEnum = user.getRole();
-            System.out.println("role = " + userRoleEnum);
-
-            Page<Product> products;
-
-            if (userRoleEnum == UserRoleEnum.USER) {
-                // 사용자 권한이 USER일 경우
-                products = productRepository.findAllByUserId(user.getId(), pageable);
-            } else {
-                products = productRepository.findAll(pageable);
-            }
-
-            return products;
-
+        if (userRoleEnum == UserRoleEnum.USER) {
+            // 사용자 권한이 USER일 경우
+            products = productRepository.findAllByUserId(user.getId(), pageable);
         } else {
-            return null;
+            products = productRepository.findAll(pageable);
         }
+
+        return products;
     }
 
     @Transactional
-    public Long updateProduct(Long id, ProductMypriceRequest requestDto, HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+    public Long updateProduct(Long id, ProductMypriceRequest requestDto, User user) {
 
-        // 토큰이 있는 경우에만 관심상품 최저가 업데이트 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
+        Product product = productRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
+                () -> new NullPointerException("해당 상품은 존재하지 않습니다.")
+        );
 
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
+        product.update(requestDto);
 
-            Product product = productRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
-                    () -> new NullPointerException("해당 상품은 존재하지 않습니다.")
-            );
+        return product.getId();
 
-            product.update(requestDto);
-
-            return product.getId();
-
-        } else {
-            return null;
-        }
     }
 
     @Transactional
-    public void updateBySearch (Long id, ItemDto itemDto){
+    public void updateBySearch(Long id, ItemDto itemDto) {
         Product product = productRepository.findById(id).orElseThrow(
                 () -> new NullPointerException("해당 상품은 존재하지 않습니다.")
         );
@@ -154,52 +83,33 @@ public class ProductService {
     }
 
     @Transactional
-    public Product addFolder(Long productId, Long folderId, HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+    public Product addFolder(Long productId, Long folderId, User user) {
 
-        // 토큰이 있는 경우에만 관심상품 최저가 업데이트 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
+        // 1) 상품을 조회합니다.
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NullPointerException("해당 상품 아이디가 존재하지 않습니다."));
 
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
+        // 2) 관심상품을 조회합니다.
+        Folder folder = folderRepository.findById(folderId)
+                .orElseThrow(() -> new NullPointerException("해당 폴더 아이디가 존재하지 않습니다."));
 
-            // 1) 관심상품을 조회합니다.
-            Product product = productRepository.findById(productId)
-                    .orElseThrow(() -> new NullPointerException("해당 상품 아이디가 존재하지 않습니다."));
-
-            // 2) 폴더를 조회합니다.
-            Folder folder = folderRepository.findById(folderId)
-                    .orElseThrow(() -> new NullPointerException("해당 폴더 아이디가 존재하지 않습니다."));
-
-            // 3) 조회한 폴더와 관심상품이 모두 로그인한 회원의 소유인지 확인합니다.
-            Long loginUserId = user.getId();
-            if (!product.getUserId().equals(loginUserId) || !folder.getUser().getId().equals(loginUserId)) {
-                throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다~^^");
-            }
-
-            Optional<Product> overLapFolder = productRepository.findByIdAndFolderList_Id(product.getId(), folder.getId());
-
-            if (overLapFolder.isPresent()) {
-               throw new IllegalArgumentException("중복된 폴더입니다.");
-            }
-
-            // 4) 상품에 폴더를 추가합니다.
-            product.addFolder(folder);
-
-            return product;
-        } else {
-            return null;
+        // 3) 조회한 폴더와 관심상품이 모두 로그인한 회원의 소유인지 확인합니다.
+        Long loginUserId = user.getId();
+        if (!product.getUserId().equals(loginUserId) || !folder.getUser().getId().equals(loginUserId)) {
+            throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다~^^");
         }
+
+        // 중복확인
+        Optional<Product> overlapFolder = productRepository.findByIdAndFolderList_Id(product.getId(), folder.getId());
+
+        if (overlapFolder.isPresent()) {
+            throw new IllegalArgumentException("중복된 폴더입니다.");
+        }
+
+        // 4) 상품에 폴더를 추가합니다.
+        product.addFolder(folder);
+
+        return product;
     }
+
 }
